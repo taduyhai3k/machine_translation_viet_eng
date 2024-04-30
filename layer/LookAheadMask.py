e726b2281ee89f548fb7b054690dca2a63ad515d
Ta Duy Hai <ta.duy.hai.fami.hust@gmail.com>
2024-04-30 23:21:13 +0700

    add device to function look_ahead_mask

--- layer/LookAheadMask.py
+++ layer/LookAheadMask.py
@@ -1,3 +1,4 @@
 import torch
 def look_ahead_mask(inp_len):
-    return 1 - torch.tril(torch.ones(inp_len, inp_len))
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    return 1 - torch.tril(torch.ones(inp_len, inp_len, device= device))
