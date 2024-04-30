2e54e8e7f05bbc042d9fad792bbf0727632c9894
Ta Duy Hai <ta.duy.hai.fami.hust@gmail.com>
2024-04-30 15:54:19 +0700

    add some function

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def SparseCrossEntropy(true, pred):
     #shape pred is [batch_size, length, embed_size]
     #shape true is [batch_size, length]
     pred_tmp = torch.gather(pred[:, :-1, :].softmax(dim = -1), dim = -1, index = true.unsqueeze(dim = -1)[:, 1:,:])
-    return torch.log(pred_tmp).sum()
+    return (torch.log(pred_tmp) * -1 ).sum() 
 
 def transformer_lr(step_num, d_model = 512, warmup_steps = 4000):
     lr = d_model ** (-0.5) * min(step_num ** (-0.5), step_num * warmup_steps ** (-1.5))
@@ -30,19 +30,91 @@ def predict(model, data, inp_tokennizer, out_tokenizer, max_lenth = 300):
             out_encode = torch.cat([out_encode, pred], dim = -1)
             if out_tokenizer.encode('<end>')[0] == predict_id:
                 return out_tokenizer.decode(out_encode.reshape(-1))
-    return out_tokenizer.decode(out_encode.reshape(-1))                
-            
-        
+    return out_tokenizer.decode(out_encode.reshape(-1))     
 
-def train(model, data_loader, optimizer, lr_schedule, epoch, device):
-    model.train()
-    scheduler = LambdaLR(optimizer, lr_lambda= lambda step_num : transformer_lr(step_num=step_num))
-    for i in range(epoch):
+def save(path, model, optimizer, scheduler, epoch):
+    state = {
+        'model' : model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'scheduler': scheduler.state_dict(),
+        'epoch': epoch
+    }           
+    torch.save(state, path)
+
+def load(path, model, optimizer, scheduler):
+    state = torch.load(path)
+    model.load_state_dict(state['model'])
+    optimizer.load_state_dict(state['optimizer'])
+    scheduler.load_state_dict(state['scheduler'])
+    scheduler.optimizer = optimizer
+    epoch = state['epoch']
+    return model, optimizer, scheduler, epoch            
+
+def eval(model, data_loader,optimizer, is_training = True):
+    mean_loss = 0        
+    infer = None
+    candidate = None    
+    device = 'gpu' if torch.cuda.is_available() else 'cpu'    
+    if is_training:
+        model.train()
         for input, target in data_loader:
             input, target = input.to(device), target.to(device)
             optimizer.zero_grad()
             output = model(input, target)
             loss = SparseCrossEntropy(target, output)
+            mean_loss += loss.item()
             loss.backward()
-            optimizer.step()
+            optimizer.step()       
+            if infer is not None:
+                infer = torch.cat([infer, output], dim = 0)
+            else:
+                infer = output
+            if candidate is not None:
+                candidate = torch.cat([candidate, target], dim = 0)   
+            else:
+                candidate = target                 
+    else:
+        model.eval()    
+        with torch.no_grad():
+            mean_loss = 0        
+            infer = None
+            candidate = None
+            for input, target in data_loader:
+                input, target = input.to(device), target.to(device)
+                output = model(input)
+                loss  = SparseCrossEntropy(target, output)
+                mean_loss += loss.item()
+                if infer is not None:
+                    infer = torch.cat([infer, output], dim = 0)
+                else:
+                    infer = output
+                if candidate is not None:
+                    candidate = torch.cat([candidate, target], dim = 0)   
+                else:
+                    candidate = target      
+    return mean_loss, bleu_score(infer, candidate)            
+                
+                    
+
+def train(path, model, optimizer, epoch, datatrain_loader,datavalid_loader = None, datatest_loader = None):
+    scheduler = LambdaLR(optimizer, lr_lambda= lambda step_num : transformer_lr(step_num=step_num))
+    tmp_score = 1e30
+    if path is not None:
+        model, optimizer, scheduler, epoch_old = torch.load(path, model, optimizer, scheduler)
+    else:
+        epoch_old = 0    
+    for i in range(epoch - epoch_old):
+        result_train =  eval(model, datatrain_loader, optimizer, True)
         scheduler.step()        
+        if datavalid_loader is not None:
+            result_valid = eval(model, datavalid_loader, optimizer, False)
+        else:
+            result_valid = [0,0]
+        if datatest_loader is not None:
+            result_test = eval(model, datatest_loader, optimizer, False)
+        else:
+            result_test = [0,0]            
+        print(f'\n Loss train {result_train[0]}, Bleu train {result_train[1]};Loss valid {result_valid[0]}, Bleu valid {result_valid[1]};Loss test {result_test[0]}, Bleu test {result_test[1]}.')    
+        if result_train[0] + result_valid[0] + result_test [0] < tmp_score:
+            save('checkpoint/bestmodel.pth', model, optimizer, scheduler)
+            tmp = result_train[0] + result_valid[0] + result_test [0]
