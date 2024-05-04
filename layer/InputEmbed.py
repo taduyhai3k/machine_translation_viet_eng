96d0dd9f6d82e043c3f59975a8d48413d05aa95d
Ta Duy Hai <ta.duy.hai.fami.hust@gmail.com>
2024-05-04 11:52:27 +0700

    fixed InputEmbed

--- layer/InputEmbed.py
+++ layer/InputEmbed.py
@@ -6,20 +6,19 @@ class InpEmbed(nn.Module):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.vocab_size = vocab_size
         self.dembed = dembed
-        self.map = torch.randn(size = [self.vocab_size, self.dembed], dtype = torch.float32, requires_grad= False, device = self.device)
-        self.map.requires_grad = True
+        self.map = torch.randn(size = [self.vocab_size, self.dembed], dtype = torch.float32, requires_grad= True, device = self.device)
     def forward(self, x):
         # x.shape is [batch, sequence_length index]   
         one_hot = torch.zeros(size = [x.shape[0], x.shape[1], self.vocab_size], dtype= torch.float32, device= self.device).scatter_(2, x.unsqueeze(2), 1.0)
-        return torch.matmul(one_hot, self.map) + embed_position(dembed= self.dembed,device= self.device)
+        return torch.matmul(one_hot, self.map) + embed_position(dembed= self.dembed, sequence_length = x.shape[1] ,device= self.device)
     
-def embed_position(dembed = 512, device = 'cpu'):
+def embed_position(dembed = 512, sequence_length = 300, device = 'cpu'):
     #dembed is 2n
-    ep = torch.arange(start= 0,end= dembed, step= 1, dtype= torch.float32, device = device)    
+    ep = torch.arange(start= 0,end= dembed, step= 1, dtype= torch.float32, device = device).repeat([sequence_length, 1])    
     ep[1::2] = ep[0::2]   
     ep = 1/ (10000**(ep/dembed)) 
-    pos = torch.arange(start= 0,end= dembed, step= 1, dtype= torch.float32, device = device)
+    pos = torch.arange(start= 0,end= sequence_length, step= 1, dtype= torch.float32, device = device).reshape([1, -1]).transpose(0,1).repeat([1, dembed])
     pos = pos * ep
-    pos[1::2] = torch.cos(pos[1::2])
-    pos[0::2] = torch.sin(pos[0::2])
+    pos[:, 1::2] = torch.cos(pos[:, 1::2])
+    pos[:, 0::2] = torch.sin(pos[:, 0::2])
     return pos
